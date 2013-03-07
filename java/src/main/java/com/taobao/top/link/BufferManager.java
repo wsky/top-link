b57cfff157527dd665e7e3ddd5bf007a9b74fd80
wsky <wskyhx@gmail.com>
2013-03-07 17:41:56 +0800

    disable directbuffer usage for travis-ci testing

--- java/src/main/java/com/taobao/top/link/BufferManager.java
+++ java/src/main/java/com/taobao/top/link/BufferManager.java
@@ -10,7 +10,11 @@ public class BufferManager {
 	public static ByteBuffer getBuffer() {
 		ByteBuffer buffer = buffers.poll();
 		// TODO:change fixed capacity
-		return buffer == null ? ByteBuffer.allocateDirect(1024 * 4) : buffer;
+		return buffer == null ? ByteBuffer.allocate(1024 * 4) : buffer;
+		// direct buffer efficiently for netty ?
+		// https://github.com/wsky/top-link/issues/12#issuecomment-14550453
+		// -XX:MaxDirectMemorySize=10m -XX:+PrintGC
+		// return buffer == null ? ByteBuffer.allocateDirect(1024 * 4) : buffer;
 	}
 
 	public static void returnBuffer(ByteBuffer buffer) {
