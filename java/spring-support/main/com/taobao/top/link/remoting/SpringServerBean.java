760cd3f1847afaecefd4b8e734069cec7b44e96f
houkun <houkun@taobao.com>
2013-05-09 15:34:05 +0800

    auto detect logging for spring

--- java/spring-support/main/com/taobao/top/link/remoting/SpringServerBean.java
+++ java/spring-support/main/com/taobao/top/link/remoting/SpringServerBean.java
@@ -13,6 +13,8 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 
 import com.taobao.top.link.BufferManager;
+import com.taobao.top.link.DefaultLoggerFactory;
+import com.taobao.top.link.LoggerFactory;
 
 public class SpringServerBean implements InitializingBean, BeanFactoryAware, ApplicationContextAware {
 	private ListableBeanFactory beanFactory;
@@ -54,12 +56,36 @@ public class SpringServerBean implements InitializingBean, BeanFactoryAware, App
 
 		RemotingConfiguration.
 				configure().
+				loggerFactory(this.getLoggerFactory()).
 				websocket(this.port).
 				addProcessor(this.path, new SpringMethodCallProcessor(this.beanFactory)).
-				businessThreadPool(new ThreadPoolExecutor(20, 
+				businessThreadPool(new ThreadPoolExecutor(20,
 						this.maxThreadCount,
 						300,
 						TimeUnit.SECONDS,
 						new SynchronousQueue<Runnable>()));
+	}
+
+	private LoggerFactory getLoggerFactory() {
+		LoggerFactory loggerFactory = null;
+
+		try {
+			Class.forName("org.apache.log4j.LogManager", 
+					false, this.getClass().getClassLoader());
+			loggerFactory = new Log4jLoggerFactory();
+		} catch (ClassNotFoundException e) {
+		}
+		
+		if (loggerFactory == null) {
+			try {
+				Class.forName("org.apache.commons.logging.Log", 
+						false, this.getClass().getClassLoader());
+				loggerFactory = new Log4jLoggerFactory();
+			} catch (ClassNotFoundException e) {
+			}
+		}
+		
+		return loggerFactory == null ? 
+				DefaultLoggerFactory.getDefault() : loggerFactory;
 	}
 }
