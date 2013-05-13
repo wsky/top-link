52d12e4644ecb2e5776e8f47dc8dbc386f24408b
houkun <houkun@taobao.com>
2013-05-13 14:03:39 +0800

    add comments to endpoint

--- java/src/main/java/com/taobao/top/link/endpoint/MessageHandler.java
+++ java/src/main/java/com/taobao/top/link/endpoint/MessageHandler.java
@@ -4,5 +4,6 @@ import java.util.HashMap;
 
 public interface MessageHandler {
 	public void onMessage(EndpointContext context) throws Exception;
+	// just got msg that can not reply
 	public void onMessage(HashMap<String, String> message);
 }
