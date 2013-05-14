cc96c1e1a0cd6325cb811be403a13c024d741d66
houkun <houkun@taobao.com>
2013-05-14 11:16:01 +0800

    update util

--- java/spring-support/main/com/taobao/top/link/remoting/Util.java
+++ java/spring-support/main/com/taobao/top/link/remoting/Util.java
@@ -1,18 +1,21 @@
 package com.taobao.top.link.remoting;
 
-import org.apache.log4j.ConsoleAppender;
-import org.apache.log4j.LogManager;
-import org.apache.log4j.PatternLayout;
+//import org.apache.log4j.ConsoleAppender;
+//import org.apache.log4j.LogManager;
+//import org.apache.log4j.PatternLayout;
 
 import com.taobao.top.link.DefaultLoggerFactory;
 import com.taobao.top.link.LoggerFactory;
 
 public class Util {
+	static {
+		//LogManager.getRootLogger().addAppender(new ConsoleAppender(new PatternLayout(PatternLayout.TTCC_CONVERSION_PATTERN)));
+	}
+
 	public static LoggerFactory getLoggerFactory(Object obj) {
 		LoggerFactory loggerFactory = null;
 		ClassLoader current = obj.getClass().getClassLoader();
 
-		LogManager.getRootLogger().addAppender(new ConsoleAppender(new PatternLayout(PatternLayout.TTCC_CONVERSION_PATTERN)));
 		try {
 			Class.forName("org.apache.log4j.LogManager", false, current);
 			loggerFactory = new Log4jLoggerFactory();
