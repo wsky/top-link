c7020fe3b7dc782fa5ff94c287ec94707d167800
houkun <houkun@taobao.com>
2013-05-20 11:05:49 +0800

    improve remoting test

--- java/embedded-websocket/test/com/taobao/top/link/remoting/RemotingWithEmbeddedClientTest.java
+++ java/embedded-websocket/test/com/taobao/top/link/remoting/RemotingWithEmbeddedClientTest.java
@@ -4,6 +4,7 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -11,22 +12,33 @@ import com.taobao.top.link.DefaultLoggerFactory;
 import com.taobao.top.link.channel.ClientChannelSelector;
 import com.taobao.top.link.channel.embedded.EmbeddedClientChannelPooledSelector;
 import com.taobao.top.link.channel.embedded.EmbeddedClientChannelSharedSelector;
+import com.taobao.top.link.channel.websocket.WebSocketServerChannel;
 
 public class RemotingWithEmbeddedClientTest {
 	private static URI uri;
+	private static WebSocketServerChannel serverChannel;
 	private static ClientChannelSelector sharedSelector = new EmbeddedClientChannelSharedSelector();
 	private static ClientChannelSelector pooledSelector = new EmbeddedClientChannelPooledSelector();
 	private static RemotingClientChannelHandler remotingHandler = new RemotingClientChannelHandler(DefaultLoggerFactory.getDefault(), new AtomicInteger(0));
 
 	@BeforeClass
 	public static void init() throws URISyntaxException {
-		uri = new URI("ws://localhost:9050/sample");
-		RemotingConfiguration.configure().websocket(uri.getPort()).addProcessor("sample", new SampleService());
+		uri = new URI("ws://localhost:8888/sample");
+		DefaultRemotingServerChannelHandler serverHandler = new DefaultRemotingServerChannelHandler();
+		serverHandler.addProcessor("sample", new SampleService());
+		serverChannel = new WebSocketServerChannel(uri.getPort());
+		serverChannel.setChannelHandler(serverHandler);
+		serverChannel.run();
+	}
+
+	@AfterClass
+	public static void clear() {
+		serverChannel.stop();
 	}
 
 	@Test
 	public void invoke_test() throws FormatterException, RemotingException {
-		new DynamicProxy(uri, sharedSelector, remotingHandler).invoke(new MethodCall());
-		new DynamicProxy(uri, pooledSelector, remotingHandler).invoke(new MethodCall());
+		((SampleInterface) new DynamicProxy(uri, sharedSelector, remotingHandler).create(SampleInterface.class, uri)).echo("hi");
+		((SampleInterface) new DynamicProxy(uri, pooledSelector, remotingHandler).create(SampleInterface.class, uri)).echo("hi");
 	}
 }
--- java/src/test/java/com/taobao/top/link/remoting/DynamicProxyTest.java
+++ java/src/test/java/com/taobao/top/link/remoting/DynamicProxyTest.java
@@ -7,37 +7,49 @@ import java.net.URISyntaxException;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.atomic.AtomicBoolean;
 
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
+
+import com.taobao.top.link.channel.websocket.WebSocketServerChannel;
 
 // high-level abstract remoting test
 public class DynamicProxyTest {
+	private static URI uri;
+	private static WebSocketServerChannel serverChannel;
+
+	@BeforeClass
+	public static void init() throws URISyntaxException {
+		uri = new URI("ws://localhost:8888/sample");
+		DefaultRemotingServerChannelHandler serverHandler = new DefaultRemotingServerChannelHandler();
+		serverHandler.addProcessor("sample", new SampleService());
+		serverChannel = new WebSocketServerChannel(uri.getPort());
+		serverChannel.setChannelHandler(serverHandler);
+		serverChannel.run();
+	}
+
+	@AfterClass
+	public static void clear() {
+		serverChannel.stop();
+	}
 
 	@Test
 	public void dynamicProxy_test() throws Throwable {
-		String uriString = "ws://localhost:9020/";
-		URI uri = new URI(uriString);
-		this.runDefaultServer(uri);
-
 		// java.lang.IllegalArgumentException:
 		// com.taobao.top.link.remoting.DynamicProxyTest$SampleService is not an
 		// interface
 		// SampleService sampleService = (SampleService)
 		// RemotingService.connect(uri, SampleService.class);
-		URI remoteUri = new URI(uriString + "sample");
-		SampleServiceInterface sampleService = (SampleServiceInterface)
-				RemotingService.connect(remoteUri, SampleServiceInterface.class);
+		SampleInterface sampleService = (SampleInterface)
+				RemotingService.connect(uri, SampleInterface.class);
 		assertEquals("hi", sampleService.echo("hi"));
 	}
 
 	@Test(expected = NullPointerException.class)
 	public void objectUri_empty_or_not_matched_processor_test() throws Throwable {
-		String uriString = "ws://localhost:9021/";
-		URI uri = new URI(uriString);
-		this.runDefaultServer(uri);
-
-		URI remoteUri = new URI(uriString + "sample_wrong");
-		SampleServiceInterface sampleService = (SampleServiceInterface)
-				RemotingService.connect(remoteUri, SampleServiceInterface.class);
+		URI remoteUri = new URI(uri.toString() + "_wrong");
+		SampleInterface sampleService = (SampleInterface)
+				RemotingService.connect(remoteUri, SampleInterface.class);
 		try {
 			sampleService.echo("hi");
 		} catch (Exception e) {
@@ -48,9 +60,6 @@ public class DynamicProxyTest {
 
 	@Test(expected = RemotingException.class)
 	public void invoke_throw_not_UndeclaredThrowable_test() throws RemotingException, Exception {
-		URI uri = new URI("ws://localhost:9022/sample");
-		this.runDefaultServer(uri);
-
 		DynamicProxy proxy = RemotingService.connect(uri);
 		MethodCall methodCall = new MethodCall();
 		methodCall.Args = new Object[] { "hi" };
@@ -60,9 +69,6 @@ public class DynamicProxyTest {
 
 	@Test
 	public void multi_thread_test() throws URISyntaxException, InterruptedException {
-		final URI uri = new URI("ws://localhost:9023/sample");
-		this.runDefaultServer(uri);
-
 		final CountDownLatch latch = new CountDownLatch(1000);
 		final AtomicBoolean flag = new AtomicBoolean(true);
 		for (int i = 0; i < 4; i++) {
@@ -71,8 +77,8 @@ public class DynamicProxyTest {
 				public void run() {
 					while (flag.get()) {
 						try {
-							SampleServiceInterface sampleService = (SampleServiceInterface)
-									RemotingService.connect(uri, SampleServiceInterface.class);
+							SampleInterface sampleService = (SampleInterface)
+									RemotingService.connect(uri, SampleInterface.class);
 							sampleService.echo("hi");
 							latch.countDown();
 						} catch (Exception e) {
@@ -84,23 +90,5 @@ public class DynamicProxyTest {
 		}
 		latch.await();
 		flag.set(false);
-	}
-
-	private RemotingConfiguration runDefaultServer(URI uri) {
-		return RemotingConfiguration.
-				configure().
-				websocket(uri.getPort()).
-				addProcessor("sample", new SampleService());
-	}
-
-	public interface SampleServiceInterface {
-		public String echo(String input);
-	}
-
-	public class SampleService extends DefaultMethodCallProcessor implements SampleServiceInterface {
-		@Override
-		public String echo(String input) {
-			return input;
-		}
 	}
 }
