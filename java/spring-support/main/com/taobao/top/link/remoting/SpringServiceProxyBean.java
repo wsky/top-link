9345b7e9871c6fc0698d92c7db12bef4d862035c
houkun <houkun@taobao.com>
2013-05-23 10:19:27 +0800

    #44;change springserviceproxy init, remove global init

--- java/spring-support/main/com/taobao/top/link/remoting/SpringServiceProxyBean.java
+++ java/spring-support/main/com/taobao/top/link/remoting/SpringServiceProxyBean.java
@@ -2,14 +2,30 @@ package com.taobao.top.link.remoting;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import org.springframework.beans.factory.FactoryBean;
 
 import com.taobao.top.link.BufferManager;
+import com.taobao.top.link.LoggerFactory;
+import com.taobao.top.link.channel.ClientChannelSelector;
+import com.taobao.top.link.channel.ClientChannelSharedSelector;
 
 // easy support spring bean
 public class SpringServiceProxyBean implements FactoryBean {
-	private static SerializationFactory serializationFactory = new CrossLanguageSerializationFactory();
+	private static SerializationFactory serializationFactory;
+	private static ClientChannelSelector channelSelector;
+	private static RemotingClientChannelHandler channelHandler;
+	static {
+		// default set 2M max message size for client
+		// TODO:change to growing buffer
+		BufferManager.setBufferSize(1024 * 1024 * 2);
+		LoggerFactory loggerFactory = Util.getLoggerFactory(new Object());
+		serializationFactory = new CrossLanguageSerializationFactory();
+		channelSelector = new ClientChannelSharedSelector(loggerFactory);
+		channelHandler = new RemotingClientChannelHandler(loggerFactory, new AtomicInteger(0));
+	}
+
 	private URI uri;
 	private Class<?> interfaceType;
 	private int executionTimeout;
@@ -37,16 +53,10 @@ public class SpringServiceProxyBean implements FactoryBean {
 
 	@Override
 	public Object getObject() throws Exception {
-		// TODO:find better way to init
-		RemotingConfiguration.configure().
-				loggerFactory(Util.getLoggerFactory(this)).
-				serializer(serializationFactory.get(this.format));
-		// default set 2M max message size for client
-		// TODO:change to growing buffer
-		BufferManager.setBufferSize(1024 * 1024 * 2);
-		DynamicProxy proxy = RemotingService.connect(this.uri);
-		if (this.executionTimeout > 0)
+		DynamicProxy proxy = new DynamicProxy(this.uri, channelSelector, channelHandler);
+		if (this.executionTimeout > 0) {
 			proxy.setExecutionTimeout(this.executionTimeout);
+		}
 		return proxy.create(this.interfaceType, this.uri);
 	}
 
