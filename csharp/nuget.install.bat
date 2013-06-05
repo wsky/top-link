5cfc9913039b689eed1dc219b3137db3088b6128
houkun <houkun@taobao.com>
2013-06-05 10:38:40 +0800

    add csharp proj

---
+++ csharp/nuget.install.bat
@@ -0,0 +1,3 @@
+del packages /F /S /Q
+
+..\..\work-tool\NuGet.exe install packages.config -NoCache -ExcludeVersion -o packages
--- submodule.cmd
+++ submodule.cmd
@@ -1,6 +1,8 @@
 #git submodule add git://github.com/wsky/RemotingProtocolParser.git java/external/RemotingProtocolParser
 #git submodule add git://github.com/wsky/websocket-client.git java/external/websocket-client
 #git checkout top-link
+#git submodule add git://github.com/wsky/websocket-sharp.git csharp/external/websocket-sharp
+#git checkout net20
 
 git submodule init
 git submodule update
