12b3ef7ccf1d0a4c02bacf1ce4dad2a47fdea2ec
houkun <houkun@taobao.com>
2013-08-29 11:21:44 +0800

    update build.sh

--- build_java.sh
+++ build_java.sh
@@ -10,8 +10,9 @@ cp java/target/*.jar build/java/
 cp lib/*.jar build/java
 
 cd tests/java
-javac -classpath ../../lib/netty-3.5.7.Final.jar:../../build/java/top-link-1.1-SNAPSHOT.jar RemotingServerTest.java
-javac -classpath ../../lib/netty-3.5.7.Final.jar:../../build/java/top-link-1.1-SNAPSHOT.jar RemotingClientTest.java
+javac -classpath ../../lib/netty-3.5.7.Final.jar:../../build/java/top-link-1.1.1-SNAPSHOT.jar RemotingServerTest.java
+javac -classpath ../../lib/netty-3.5.7.Final.jar:../../build/java/top-link-1.1.1-SNAPSHOT.jar RemotingClientTest.java
+javac -classpath ../../build/java/top-link-1.1.1-SNAPSHOT.jar RemotingClientEmbeddedTest.java
 cp *.class ../../build/java
 cp *.bat ../../build/java
 cp *.sh ../../build/java
