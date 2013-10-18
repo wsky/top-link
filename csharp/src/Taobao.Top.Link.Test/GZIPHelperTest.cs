7a7881d0d57e87654bba94743227913161ffcbde
houkun <houkun@taobao.com>
2013-10-18 17:48:59 +0800

    #83;add c# gziphelper;test compatiable

---
+++ csharp/src/Taobao.Top.Link.Test/GZIPHelperTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using Taobao.Top.Link.Util;
+
+namespace Taobao.Top.Link.Test
+{
+    [TestFixture]
+    public class GZIPHelperTest
+    {
+        [TestCase]
+        public void ZipTest()
+        {
+            var str = "hi";
+            var data = Encoding.UTF8.GetBytes(str);
+            Console.WriteLine(data.Length);
+            var zipped = GZIPHelper.Zip(data);
+
+            Console.WriteLine(zipped.Length);
+            foreach (var b in zipped)
+                Console.Write(b + ",");
+            Console.WriteLine();
+
+            Assert.AreEqual(str, Encoding.UTF8.GetString(GZIPHelper.Unzip(zipped)));
+        }
+
+        [TestCase]
+        public void UnzipTest()
+        {
+            //from java gzip
+            var data = new byte[] { 31, (-117) + 256, 8, 0, 0, 0, 0, 0, 0, 0, (-53) + 256, (-56) + 256, 4, 0, (-84) + 256, 42, (-109) + 256, (-40) + 256, 2, 0, 0, 0 };
+            var str = Encoding.UTF8.GetString(GZIPHelper.Unzip(data));
+            Console.WriteLine(str);
+            Assert.AreEqual("hi", str);
+        }
+    }
+}
